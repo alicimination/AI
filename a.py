648310a4d285fc43f4ebd23a70e94cf636a7600d
alicimination <114404758+alicimination@users.noreply.github.com>
2026-03-08 11:00:59 +0000

    Delete a.py

--- a.py
+++
@@ -1,9 +0,0 @@
-import os
-os.environ["PADDLE_PDX_DISABLE_MODEL_SOURCE_CHECK"] = "True"
-from paddleocr import PaddleOCR
-
-ocr = PaddleOCR(use_angle_cls=True, lang='en')
-
-result = ocr.predict("test.jpg")
-
-print(result)
